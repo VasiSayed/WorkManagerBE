ecd0ef2cb1e8b03c6418361fbc51384fcd5518d8
VasihSayed <vasisayed09421@gmail.com>
2026-06-15 22:47:16 +0530

    email

--- config/settings.py
+++ config/settings.py
@@ -106,3 +106,11 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'vasisayed09421@gmail.com'
+EMAIL_HOST_PASSWORD = 'zfwl rmkv nawj hiak'
